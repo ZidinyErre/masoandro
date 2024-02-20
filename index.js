867beed7e00f9e37cd5d58606a7b64785ea543d3
Zidiny Erre <zidiny.erre.fr@gmail.com>
2024-02-21 00:30:14 +0100

    Essai de connexion à l'API

---
+++ index.js
@@ -0,0 +1,20 @@
+// window.addEventListener("load", function () {
+//     console.log(" bien ouej !!");
+// })
+
+// const apiUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=d3c63080f29514c09fe0fbe19103afd7";
+// console.log(apiUrl);
+
+
+// fetch(apiUrl)
+// .then(response =>{
+//     if (!response.ok) {
+//         throw new Error('Erreur de réseau lors de la requête!');
+//     }
+
+//     return response.json()
+// })
+// .then(data =>{
+//     console.log( "données météo : ", data);
+// })
+// .catch(error =>{console.error('Erreur lors de la récupérations des données!',error);})
