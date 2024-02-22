0e5f26ad48a923f5db2919e0b978c3f2bc6a8949
Zidiny Erre <zidiny.erre.fr@gmail.com>
2024-02-22 01:37:42 +0100

    Connexion à l API meteo réussi parfaitement

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // window.addEventListener("load", function () {
 //     console.log(" bien ouej !!");
 // })
-
-// const apiUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=d3c63080f29514c09fe0fbe19103afd7";
+// // const apiUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=d3c63080f29514c09fe0fbe19103afd7";
+// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=montpellier&appid=d3c63080f29514c09fe0fbe19103afd7&units=metric";
 // console.log(apiUrl);
 
 
