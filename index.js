bb4a52f1212822a278ebef8061ec34c0039e3591
Zidiny Erre <zidiny.erre.fr@gmail.com>
2024-02-25 21:31:35 +0100

    Problème lié à webpack et les variables d'environnement

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-// window.addEventListener("load", function () {
-//     console.log(" bien ouej !!");
-// })
-// require('dotenv').config();
-// const apiKey = process.env.KEY_API;
-// const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=montpellier&appid=${apiKey}&units=metric";
-// console.log(apiUrl);
+window.addEventListener("load", function () {
+    console.log(" bien ouej !!");
+})
+const apiKey = process.env.KEY_API;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=montpellier&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
 
 
-// fetch(apiUrl)
-// .then(response =>{
-//     if (!response.ok) {
-//         throw new Error('Erreur de réseau lors de la requête!');
-//     }
+fetch(apiUrl)
+.then(response =>{
+    if (!response.ok) {
+        throw new Error('Erreur de réseau lors de la requête!');
+    }
 
-//     return response.json()
-// })
-// .then(data =>{
-//     console.log( "données météo : ", data);
-// })
-// .catch(error =>{console.error('Erreur lors de la récupérations des données!',error);})
+    return response.json()
+})
+.then(data =>{
+    console.log( "données météo : ", data);
+})
+.catch(error =>{console.error('Erreur lors de la récupérations des données!',error);})
 
+const meteo = document.querySelector("#meteo");
+meteo.innerHTML = data.timezone;
